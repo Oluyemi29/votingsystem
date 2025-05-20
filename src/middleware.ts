17dce424a0cf7e936bf48af9c48760ac240d60bc
Oluyemi29 <adedokunoluyemi29@gmail.com>
2025-05-20 07:59:10 +0100

    correction

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,10 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req });
+  const token = await getToken({
+    req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
   const { pathname } = req.nextUrl;
   if (pathname.startsWith("/admin")) {
     if (pathname !== "/admin/login" && pathname !== "/admin/register") {
